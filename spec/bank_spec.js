6f4f4e17d501f1c02f4f2d5c9d85fbf9f37595f4
alakijayo <alakijaayo@gmail.com>
2018-11-09 17:20:17 +0000

    Edge Testing and understanding Mocha/Chai

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -1,5 +1,5 @@
 const mocha = require('mocha');
-const chai = require('chai');
+const expect = require('chai').expect;
 const Bank = require('../src/bank');
 var assert = require('chai').assert;
 
@@ -19,4 +19,25 @@ it('starts with a balance of 0', function() {
 it('shows your balance when requested', function() {
   bank.deposit(50)
   assert.equal(bank.myBalance(), 50);
-}) ;
+})
+
+it('raises an error if amount put in deposit is negative', function() {
+  expect(function () { bank.deposit(-50) }).to.throw("Please put in correct amount you wish to deposit.");
+})
+
+it('raises an error if amount put in withdraw is negative', function() {
+  expect(function () { bank.withdraw(-50) }).to.throw("Please put in correct amount you wish to withdraw.");
+})
+
+it('raises an error if amount put in deposit is 0', function() {
+  expect(function () { bank.deposit(0) }).to.throw("Please put in correct amount you wish to deposit.")
+})
+
+it('raises an error if amount put in withdraw is 0', function() {
+  expect(function () { bank.withdraw(0) }).to.throw("Please put in correct amount you wish to withdraw.")
+})
+
+it('raises an error if limit on account is exceeded', function() {
+  bank.deposit(20)
+  expect(function () { bank.withdraw(30) }).to.throw("Not enough funds available. Please put in amount you wish to withdraw.")
+})
--- src/bank.js
+++ src/bank.js
@@ -8,10 +8,18 @@ Bank.prototype.myBalance = function() {
 };
 
 Bank.prototype.deposit = function(money) {
+  if (money <= 0 || typeof money === 'string') {
+    throw "Please put in correct amount you wish to deposit.";
+  }
   return this.balance += money;
 };
 
 Bank.prototype.withdraw = function(money) {
+  if (money <= 0 || typeof money === 'string') {
+    throw "Please put in correct amount you wish to withdraw.";
+  } else if (this.balance - money <= this.MINIMUM_BALANCE) {
+    throw "Not enough funds available. Please put in amount you wish to withdraw.";
+  }
   return this.balance -= money;
 };
 
