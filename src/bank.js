107bedc9b4de6f706144bb1bb4c666edcd60dd4d
alakijayo <alakijaayo@gmail.com>
2018-11-27 11:40:18 +0000

    implementing table function into bank
    replacing and updating the bank function to use table function to store, update and receive most data

--- src/bank.js
+++ src/bank.js
@@ -1,33 +1,32 @@
+const Table = require('../src/table')
+
 function Bank() {
   this.MINIMUM_BALANCE = 0;
-  this.balance = this.MINIMUM_BALANCE;
-  this.table = new Table
+  this.table = new Table();
 }
 
 Bank.prototype.myBalance = function() {
-  return this.balance;
+  return this.table.balance;
 };
 
 Bank.prototype.deposit = function(money) {
   if (money <= 0 || typeof money === 'string') {
     throw "Please put in correct amount you wish to deposit.";
   }
-  this.table.deposit(money)
-  return this.balance += money;
+  return this.table.deposit(money);
 };
 
 Bank.prototype.withdraw = function(money) {
   if (money <= 0 || typeof money === 'string') {
     throw "Please put in correct amount you wish to withdraw.";
-  } else if (this.balance - money <= this.MINIMUM_BALANCE) {
+  } else if (this.table.balance - money <= this.MINIMUM_BALANCE) {
     throw "Not enough funds available. Please put in amount you wish to withdraw.";
   }
-  this.table.withdraw(money)
-  return this.balance -= money;
+  return this.table.withdraw(money);
 };
 
 Bank.prototype.showTable = function () {
-  return this.table.display
+  return this.table.display();
 };
 
 module.exports = Bank
