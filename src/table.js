531847093cae66580e471f5708bf041d8b3c23ae
alakijayo <alakijaayo@gmail.com>
2018-11-27 10:52:40 +0000

    Table Updated
    Got table to print out a console.log version of the bank tech table as given by the requirements

--- src/table.js
+++ src/table.js
@@ -6,19 +6,19 @@ function Table() {
 Table.prototype.deposit = function (money) {
   var date = new Date
   var gb = new Intl.DateTimeFormat('en-GB').format(date);
-  this.history.push(`${date} || ${money} || || ${this.balance += money}`)
+  this.history.push([`${gb} || ${money} || || ${this.balance += money}`])
   return "Your money has been deposited!"
 };
 
 Table.prototype.withdraw = function (money) {
   var date = new Date;
   var gb = new Intl.DateTimeFormat('en-GB').format(date);
-  this.history.push(`${gb} || || ${-money} || ${this.balance -= money}`)
+  this.history.push([`${gb} || || ${-money} || ${this.balance -= money}`])
   return "Your money has been withdrawn!"
 };
 
 Table.prototype.display = function () {
-  return ("date || credit || debit || balance" + `${this.history}`)
+  return console.log("date || credit || debit || balance " + "\n" + `${this.history.join('\n')}`)
 };
 
 module.exports = Table
